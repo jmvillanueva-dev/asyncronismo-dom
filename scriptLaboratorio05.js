09c8567eafdd85c1dfb8286e706d68e564959661
Jhonny Villanueva Montoya <jhonny.villanueva@epn.edu.ec>
2025-05-08 13:17:24 -0500

    Actualizado script para Laboratorio05

--- scriptLaboratorio05.js
+++ scriptLaboratorio05.js
@@ -1,1 +1,34 @@
-<html><head><meta content="text/html; charset=UTF-8" http-equiv="content-type"><title>Gmail: Error de descarga</title></head><body>Por razones de seguridad, Gmail no te permite utilizar este tipo de archivo porque infringe la política de Google sobre archivos ejecutables y comprimidos.</body></html>
+/*
+    Laboratorio # 5
+    1. Adrian Caiza
+    2. Erick Nuñez
+    3. Jhonny Villanueva M.
+*/
+
+// PAGINA WEB DEWALT 
+
+// Primera forma de trabajar con fetch()
+fetch('https://dwstore.ec/?s=Taladro&post_type=product&type_aws=true&aws_id=1&aws_filter=1')
+    .then( r => r.json() )
+    .then( p => console.log(p))
+    .catch( e => console.log(e))
+
+
+// Segunda forma de trabajar con fetch()
+
+// Creamos una variables para guardar la dirreccion de la Api
+let productos = 'https://dwstore.ec/?s=Taladro&post_type=product&type_aws=true&aws_id=1&aws_filter=1';
+
+// Creamos la funcion para obtener los productos o extraer los productos desde una api
+const obtenerProductos = async () => {
+    try {
+        const response = await fetch(productos);
+        const p = await response.json();
+        console.log(p);
+    }catch (e) {
+        console.log(e)
+    }
+}
+
+// Utilizamos la funcion obtener productos
+obtenerProductos();
